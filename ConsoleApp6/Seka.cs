55ccdb2d52c8bdfea05166864a2c4f279cc4cea9
kostasva@gmail.com <kostasva@gmail.com>
2022-03-04 13:47:09 +0200

    Pakeistas sekos pradinio skaiciaus kintamojo raide

--- ConsoleApp6/Seka.cs
+++ ConsoleApp6/Seka.cs
@@ -8,8 +8,7 @@ namespace Paskaita2
 {
     internal class Matematika : Faktorialas
     {
-
-        public int a = 2; // startinis skaicius
+        public int x = 2; // pradinis skaicius
         public int r = 2; // Kas kiek didint
         public double n = 1000000000; // Iki kiek skaiciuot
         public double sk; //Skaiciuojamasis kintamasis
@@ -23,7 +22,7 @@ namespace Paskaita2
 
             for (int i = 0; sk < n; i++)
             {
-                sk = a * (int)Math.Pow(r, i);
+                sk = x * (int)Math.Pow(r, i);
 
                 k = i;
 
